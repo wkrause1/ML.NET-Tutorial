25106a6dde962973b83bd6223d97a940429e007c
Will Krause <William.Krause-contractor@nreca.coop>
2020-02-21 15:50:46 -0500

    Initial commit of completed ML.NET app

---
+++ myMLApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MyMLAppML.Model;
+
+namespace myMLApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Add input data
+            var input = new ModelInput();
+            input.SentimentText = "I appreciate all the work you've done so far. It has been a great help.";
+
+            // Load model and predict output of sample data
+            ModelOutput result = ConsumeModel.Predict(input);
+            Console.WriteLine($"Text: {input.SentimentText}\nIs Toxic: {result.Prediction}");
+        }
+    }
+}
